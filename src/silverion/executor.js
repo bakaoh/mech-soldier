9bede9937dbf7c735a9e8c55072a861d36408291
Bakaoh <batatsar@gmail.com>
2022-07-02 17:12:30 +0700

    Add executor

---
+++ src/silverion/executor.js
@@ -0,0 +1,61 @@
+const Web3 = require("web3");
+const Tx = require('ethereumjs-tx').Transaction;
+const Common = require('ethereumjs-common');
+const Silverion = require('../abi/Silverion.json');
+
+const common = Common.default.forCustomChain('mainnet', {
+    name: 'bnb',
+    networkId: 56,
+    chainId: 56,
+}, 'istanbul');
+
+const web3 = new Web3("https://bsc-dataseed.binance.org");
+web3.eth.handleRevert = true;
+
+const SilverionContract = new web3.eth.Contract(Silverion.abi, Silverion.address);
+
+class Executor {
+    constructor(address, key) {
+        this.address = web3.utils.toChecksumAddress(address);
+        this.key = key;
+    }
+
+    async execute(amountIn, routers, path1, path2) {
+        const data = SilverionContract.methods.executeOrder(
+            web3.utils.toHex(amountIn),
+            routers.map(a => web3.utils.toChecksumAddress(a)),
+            path1.map(a => web3.utils.toChecksumAddress(a)),
+            path2.map(a => web3.utils.toChecksumAddress(a))
+        ).encodeABI();
+        return this.sendTx(COINMAPDEX_ADDRESS, data);
+    }
+
+    async sendBnb(toAddress, amount) {
+        return this.sendTx(toAddress, undefined, web3.utils.toHex(amount));
+    }
+
+    getBnbBalance() {
+        return web3.eth.getBalance(this.address);
+    }
+
+    async sendTx(to, data = undefined, value = '0x') {
+        const txCount = await web3.eth.getTransactionCount(this.address);
+        var txObject = {};
+        txObject.nonce = web3.utils.toHex(txCount);
+        txObject.gasLimit = web3.utils.toHex(data ? 1000000 : 21000);
+        txObject.gasPrice = web3.utils.toHex(web3.utils.toWei("5", "gwei"));
+        txObject.to = web3.utils.toChecksumAddress(to);
+        txObject.value = value;
+        if (data) txObject.data = data;
+
+        //Sign transaction before sending
+        var tx = new Tx(txObject, { common });
+        var privateKey = Buffer.from(this.key, 'hex')
+        tx.sign(privateKey);
+        var serializedTx = tx.serialize();
+        return web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex'))
+            .then(tx => tx.transactionHash);
+    }
+}
+
+module.exports = Executor;
--- src/silverion/sync.js
+++ src/silverion/sync.js
@@ -2,10 +2,19 @@ const Crawler = require("../utils/crawler");
 const { web3, ContractAddress, toBN, getFactoryName } = require('../utils/bsc');
 const { Partitioner, getLastLine, getLastFile, getLastFiles } = require('../utils/io');
 const { getNumber } = require('../utils/format');
+const Executor = require('./executor');
 
 const SYNC_TOPIC = '0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1';
 const BLOCK_FILE = 'logs/silverion.block';
 const ZERO = toBN(0);
+
+const supportQuote = [ContractAddress.WBNB]
+
+const factory2Router = {
+    [ContractAddress.PANCAKE_FACTORY]: ContractAddress.PANCAKE_ROUTER,
+    [ContractAddress.BISWAP_FACTORY]: ContractAddress.BISWAP_ROUTER,
+    [ContractAddress.APESWAP_FACTORY]: ContractAddress.APESWAP_ROUTER,
+}
 
 const factoryInWithFee = {
     [ContractAddress.PANCAKE_FACTORY]: 9975,
@@ -24,6 +33,8 @@ class SyncModel {
     constructor(pairModel) {
         this.pairModel = pairModel;
         this.reserves = {};
+        this.executor = new Executor(process.env.SILVERION_ADDR, process.env.SILVERION_KEY);
+        this.lastExecute = {};
     }
 
     async runCrawler() {
@@ -44,20 +55,34 @@ class SyncModel {
         const priceB = this.calcPrice(this.reserves[token0][token1][factoryB]);
         if (priceA == 0 || priceB == 0) return;
         if (Math.abs(priceA - priceB) * 100 < priceA) return;
-        if (ContractAddress.BUSD == token1 || ContractAddress.USDT == token1 || ContractAddress.WBNB == token1) {
+        if (supportQuote.includes(token1)) {
             const amountIn = ContractAddress.WBNB != token1 ? toBN("200000000000000000000") : toBN("1000000000000000000");
             const [factory1, factory2] = priceA < priceB ? [factoryA, factoryB] : [factoryB, factoryA];
             const amountOut1 = getAmountOut(factory1, amountIn, this.reserves[token0][token1][factory1][1], this.reserves[token0][token1][factory1][0]);
             const amountOut2 = getAmountOut(factory2, amountOut1, this.reserves[token0][token1][factory2][0], this.reserves[token0][token1][factory2][1]);
             const ok = amountOut2.gt(amountIn);
-            console.log(`Found(${ok}) [${token0},${token1}] ${amountIn.toString(10)} ${getFactoryName(factory1)}=>${amountOut2.toString(10)} ${getFactoryName(factory2)}`);
-        } else if (ContractAddress.BUSD == token0 || ContractAddress.USDT == token0 || ContractAddress.WBNB == token0) {
+            console.log(`Found(${ok}) [${token0}] ${amountIn.toString(10)} ${getFactoryName(factory1)}=>${amountOut2.toString(10)} ${getFactoryName(factory2)}`);
+            if (ok) {
+                if (this.lastExecute[`${token0}-${token1}`] && (Date.now() - this.lastExecute[`${token0}-${token1}`] < 5000)) return;
+                this.lastExecute[`${token0}-${token1}`] = Date.now();
+                const routers = [factory2Router(factory1), factory2Router(factory2)];
+                const tx = await this.executor.execute(amountIn.toString(10), routers, [token1, token0], [token0, token1]);
+                console.log(`Execute`, tx);
+            }
+        } else if (supportQuote.includes(token0)) {
             const amountIn = ContractAddress.WBNB != token0 ? toBN("200000000000000000000") : toBN("1000000000000000000");
             const [factory1, factory2] = priceA > priceB ? [factoryA, factoryB] : [factoryB, factoryA];
             const amountOut1 = getAmountOut(factory1, amountIn, this.reserves[token0][token1][factory1][0], this.reserves[token0][token1][factory1][1]);
             const amountOut2 = getAmountOut(factory2, amountOut1, this.reserves[token0][token1][factory2][1], this.reserves[token0][token1][factory2][0]);
             const ok = amountOut2.gt(amountIn);
-            console.log(`Found(${ok}) [${token0},${token1}] ${amountIn.toString(10)} ${getFactoryName(factory1)}=>${amountOut2.toString(10)} ${getFactoryName(factory2)}`);
+            console.log(`Found(${ok}) [${token1}] ${amountIn.toString(10)} ${getFactoryName(factory1)}=>${amountOut2.toString(10)} ${getFactoryName(factory2)}`);
+            if (ok) {
+                if (this.lastExecute[`${token0}-${token1}`] && (Date.now() - this.lastExecute[`${token0}-${token1}`] < 5000)) return;
+                this.lastExecute[`${token0}-${token1}`] = Date.now();
+                const routers = [factory2Router(factory1), factory2Router(factory2)];
+                const tx = await this.executor.execute(amountIn.toString(10), routers, [token0, token1], [token1, token0]);
+                console.log(`Execute`, tx);
+            }
         }
     }
 
