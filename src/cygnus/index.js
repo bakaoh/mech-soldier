fa69a0530482dfbec1ac149a3e9e8b92d0983d92
Bakaoh <batatsar@gmail.com>
2022-09-10 16:13:36 +0700

    Add telegram bot

--- src/cygnus/index.js
+++ src/cygnus/index.js
@@ -1,11 +1,25 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
+
 const express = require("express");
-const axios = require("axios");
+const TelegramBot = require("node-telegram-bot-api");
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+bot.on("polling_error", (e) => console.log(JSON.stringify(e)));
+bot.on("message", (msg) => {
+    console.log(JSON.stringify(msg));
+});
 
 const app = express();
 app.use(express.json());
 
+const watch = [-1001754164580];
 app.post('/check', async (req, res) => {
-    console.log(req.body)
+    if (watch.includes(req.body.chat_id)) {
+        console.log(req.body);
+        bot.sendMessage(chatId, req.body.text, { parse_mode: "HTML" }).catch(console.log);
+    }
+
     res.json({ error: 0 });
 })
 
